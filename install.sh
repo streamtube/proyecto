d0fe37934d5e5501aa95945740730f285d7b066c
Tomás <tomas2387@gmail.com>
2013-11-04 17:55:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,12 +17,7 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java7-installer
 
-PHPSTORM_HOME="$HOME/phpstorm"
-DESKTOP_ENTRY_FILE_NAME="PhpStorm.desktop"
-
-mkdir -p $PHPSTORM_HOME
-cd $PHPSTORM_HOME
-
+cd $HOME
 wget http://download.jetbrains.com/webide/PhpStorm-7.0.tar.gz -O PhpStorm.tar.gz
 
 # get directory to which will be PhpStorm extracted
@@ -32,9 +27,12 @@ PHPSTORM_DIR=`tar -ztf PhpStorm.tar.gz | grep "bin/phpstorm.sh" | tail -n 1 | aw
 tar -zxvf PhpStorm.tar.gz
 rm PhpStorm.tar.gz
 
+mv $PHPSTORM_DIR phpstorm
+
+ln -s /usr/bin/phpstorm $HOME/phpstorm/bin/phpstorm.sh
+
 echo -e "Done Phpstorm."
 
-ln -s /usr/bin/phpstorm ~/$PHPSTORM_DIR/bin/phpstorm.sh
 
 mkdir /tmp/node-install
 cd /tmp/node-install
@@ -51,6 +49,8 @@ cd $NODE_DIR
 ./configure && make && make install
 echo 'Node.js install completed'
 
+
+echo "iniciando proyecto webrtc"
 cd /var/www/proyecto/
 
 firefox http://localhost/proyecto/WEBRTC.html
