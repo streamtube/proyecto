fe575f080f981364cd33a009008ebab92287d639
Ninjacu <piratapiton@gmail.com>
2013-12-08 20:07:31 +0100

    webrtc ejemplos faltaba el link

--- video-conferencing/videodeytb.js
+++ video-conferencing/videodeytb.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 $("#botoncrear").on("click",function () {
     var variable = $("#youtubeid").val();
-    var url = "www.youtube.com/embed/" + variable;
+    var url = "http://www.youtube.com/embed/" + variable;
     $("#videoiframe").attr("src", url);
 });
 });
