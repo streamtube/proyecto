f2cdfd7e3b6350541d8d7ccfdaf3f29c245269d9
Ninjacu <piratapiton@gmail.com>
2013-12-08 19:54:33 +0100

    webrtc ejemplos faltaba el link

--- video-conferencing/videodeytb.js
+++ video-conferencing/videodeytb.js
@@ -1,4 +1,4 @@
-$("#botoncrear").on("click", function () {
+$("#botoncrear").on("click",function () {
     var variable = $("#youtubeid").val();
     var url = "http://youtube.com/watch?v=" + variable;
     $("#videoiframe").attr("src", url);
