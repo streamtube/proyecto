3c198628382f2f17c487fb285d533cc1aed3adbb
tomas2387 <tomas2387@gmail.com>
2013-12-10 01:21:18 +0100

    Agregado servidor socket io

--- server.js
+++ server.js
@@ -1,5 +1,50 @@
+var server  = require('http').createServer(),
+    sio      = require('socket.io'),
+    port    = 8081;
+server.listen(port);
+var io = sio.listen(server, { log:true });
+
+var channels = {};
+
 io.sockets.on('connection', function (socket) {
-    socket.on('message', function (data) {
-        socket.broadcast.emit('message', data);
+    console.log("Cliente conectado");
+    var initiatorChannel = '';
+    if (!io.isConnected)
+        io.isConnected = true;
+
+    socket.on('new-channel', function (data) {
+        channels[data.channel] = data.channel;
+        console.log("Nuevo canal", data);
+        onNewNamespace(data.channel, data.sender);
+    });
+
+    socket.on('presence', function (channel) {
+        var isChannelPresent = !! channels[channel];
+        socket.emit('presence', isChannelPresent);
+        if (!isChannelPresent)
+            initiatorChannel = channel;
+    });
+
+    socket.on('disconnect', function (channel) {
+        console.log("Cliente desconectado");
+        if (initiatorChannel)
+            channels[initiatorChannel] = null;
     });
 });
+
+function onNewNamespace(channel, sender) {
+    io.of('/' + channel).on('connection', function (socket) {
+        if (io.isConnected) {
+            io.isConnected = false;
+            socket.emit('connect', true);
+        }
+
+        socket.on('message', function (data) {
+            if (data.sender == sender)
+                socket.broadcast.emit('message', data.data);
+        });
+    });
+}
+
+console.log('1- Escuchando en http://localhost:' + port , "");
+console.log("");
