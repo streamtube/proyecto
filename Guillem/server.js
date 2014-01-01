feb4a729794e6931cc50ef27bc9e680d9bfaf1ae
GuillemV98 <98Guill398@gmail.com>
2014-01-01 14:55:39 +0100

    guillem

--- Guillem/server.js
+++ Guillem/server.js
@@ -1,15 +1,15 @@
 /**
- * Created by root on 12/30/13.
+ * Created by root on 1/1/14.
  */
+
 var server  = require('http').createServer(),
     sio      = require('socket.io'),
     port    = 8082;
 server.listen(port);
 var io = sio.listen(server, { log:true });
-
+var channels = {};
 io.sockets.on('connection', function (socket) {
     console.log("Cliente conectado");
-
     socket.on('mensaje', function (msg) {
         console.log(msg);
         socket.broadcast.emit('mensaje', msg);
