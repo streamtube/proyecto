f83834dfae6c5560f3879f1e65a52bddd2360d0a
Ninjacu <javijakaton555@gmail.com>
2014-01-01 15:00:52 +0100

    d

--- Guillem/client.js
+++ Guillem/client.js
@@ -2,34 +2,21 @@
  * Created by root on 1/1/14.
  */
 
-var server  = require('http').createServer(),
-    sio      = require('socket.io'),
-    port    = 8082;
-server.listen(port);
-var io = sio.listen(server, { log:true });
-var channels = {};
-io.sockets.on('connection', function (socket) {
-    console.log("Cliente conectado");
-});
-    console.log('1- Escuchando en http://localhost:' + port , "");
-    console.log("");
 
 $(document).ready(function() {
     var urlServer = location.origin + ':8082';
     var socket = io.connect(urlServer);
 
-
     $("#boton").on('click', function() {
         var mensaje = $("#write").val();
+        console.log(mensaje);
         socket.emit("mensaje", {msg: mensaje});
+
     });
-    socket.on("mensaje", function(msg) {
-        console.log("hemos recibido un mensaje", msg);
-    });
+
     socket.on("mensaje", function(msg) {
         console.log("hemos recibido un mensaje", msg);
         var mensajeReal = msg.msg;
-        $( "#mensajes" ).append( "<p>+mensajeReal+</p>" );
+        $( "#mensajes" ).append( "<p>"+mensajeReal+"</p>" );
     });
-
 });
--- Guillem/server.js
+++ Guillem/server.js
@@ -1,15 +1,15 @@
 /**
  * Created by root on 1/1/14.
  */
-
 var server  = require('http').createServer(),
     sio      = require('socket.io'),
     port    = 8082;
 server.listen(port);
 var io = sio.listen(server, { log:true });
-var channels = {};
+
 io.sockets.on('connection', function (socket) {
     console.log("Cliente conectado");
+
     socket.on('mensaje', function (msg) {
         console.log(msg);
         socket.broadcast.emit('mensaje', msg);
@@ -17,3 +17,4 @@ io.sockets.on('connection', function (socket) {
 });
 console.log('1- Escuchando en http://localhost:' + port , "");
 console.log("");
+
