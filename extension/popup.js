2897211d3e08eb4aa7e75acfdc308178e00799f6
GuillemV98 <98Guill398@gmail.com>
2014-03-22 12:57:32 +0100

    Merge branch 'master' of https://github.com/streamtube/proyecto
    Conflicts:
    	recursos/css/estilo.css

---
+++ extension/popup.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Global variable containing the query we'd like to pass to Flickr. In this
+ * case, kittens!
+ *
+ * @type {string}
+ */
+var QUERY = 'stextension';{
+
+
+
+
+};
+
+// Run our kitten generation script as soon as the document's DOM is ready.
+document.addEventListener('DOMContentLoaded', function () {
+;
+});
